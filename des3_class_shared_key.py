647c5b3e3b65b40dbbf2ee333a85292804bb1a7f
William Loring <32806514+itinstructor@users.noreply.github.com>
2023-09-29 23:05:29 -0600

    Update des3 class

---
+++ des3_class_shared_key.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+"""
+    Name: des3_shared_key.py
+    Author: 
+    Created: 09/29/2023
+    Purpose: 3DES encryption demonstration using user input shared key
+    3DES is considered a weak encryption algorithm and is not recommended
+    for secure applications. More modern encryption algorithms like AES
+    should be used for security-critical applications.
+    It uses a 192 bit (24 bytes) key.
+    Like DES, it uses an 8 byte block cipher.
+    Data is encrypted 8 bytes at a time
+    Data is padded to increments of 8 bytes.
+"""
+# pip install pycryptodome
+from Crypto.Cipher import DES3
+# Import hashlib for secure key derivation from shared key from user
+import hashlib
+
+
+class DES3Class:
+    def __init__(self, plain_text: str, shared_key="": str) -> None:
+        self.plain_text = plain_text
+        self.shared_key = shared_key
+
+    def encrypt(self):
+        """Encrypt plain text with 3DES"""
+        # Pad the key to 24 bytes using SHA-256 hash for compatibility with 3DES
+        # Triple DES (3DES)  requires an 24-byte key,
+        # We derive a 24-byte from the user-provided key
+
+        # Create a hash of the user-provided key using SHA-256
+        hashed_key = hashlib.sha256(self.shared_key.encode())
+        hashed_key_bytes = hashed_key.digest()
+
+        # Take the first 24 bytes of the hash to
+        # create a valid 24-byte 3DES key
+        self.hashed_key_bytes = hashed_key_bytes[:24]
+
+        # Create a DES cipher object with the derived key
+        # in ECB (Electronic Codebook) mode
+        self.cipher = DES3.new(self.hashed_key_bytes, DES3.MODE_ECB)
+
+        # Convert (encode) message string to bytes
+        message_bytes = self.plain_text.encode()
+        # Ensure that the message length is a multiple
+        # of 8 bytes (required by DES)
+        while len(message_bytes) % 8 != 0:
+            # Pad the message with spaces if needed
+            message_bytes += b' '
+
+        # Use the DES cipher to encrypt the padded message
+        self.encrypted_data = self.cipher.encrypt(message_bytes)
+
+    def decrypt(self):
+        """Decrypt DES3 encrypted data to plain text"""
+        # Use the same 3DES cipher to decrypt the encrypted message
+        decrypted_data_bytes = self.cipher.decrypt(self.encrypted_data)
+
+        # Remove any trailing spaces from the decrypted message
+        decrypted_data_bytes = decrypted_data_bytes.rstrip(b' ')
+
+        # Decode bytes to a string
+        decrypted_data = decrypted_data_bytes.decode('utf-8')
+        self.decrypted_data = decrypted_data
+
+
+def main():
+    """Test 3DES encryption module"""
+    # Get user input for the message and the shared key
+    plain_text = input("Text to encrypt: ")
+    shared_key = input("Shared key: ")
+    # Create object with plain text and shared key input arguments
+    cd = DES3Class(plain_text, shared_key)
+
+    cd.encrypt()
+    cd.decrypt()
+
+    # Print results
+    print(f"  Original Text: {cd.plain_text}")
+    print(f"     Shared Key: {cd.hashed_key_bytes.hex()}")
+    print(f" Encrypted Data: {cd.encrypted_data.hex()}")
+    print(f" Decrypted Text: {cd.decrypted_data} ")
+
+
+# If a standalone program, call the main function
+# Else, use as a module
+if __name__ == "__main__":
+    main()
